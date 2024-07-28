9f5834e543231f1868d8b868113abe6a356d8c1c
FreshDoktor <freshdoktor@freshgaming.eu>
2024-07-28 18:23:21 +0200

    add logging to events

--- code/Components/GameLoop/GameStateManager.cs
+++ code/Components/GameLoop/GameStateManager.cs
@@ -16,26 +16,31 @@ public sealed class GameStateManager : Component, IGameEventHandler<GameEndEvent
 
 	public void OnGameEvent( GameEndEvent eventArgs )
 	{
+		Log.Info("Event: " + "GameEndEvent");
 		CurrentState = UiManager.ChangeState(GameState.END_GAME);
 	}
 
 	public void OnGameEvent( GameStartEvent eventArgs )
 	{
+		Log.Info("Event: " + "GameStartEvent");
 		CurrentState = UiManager.ChangeState(GameState.INGAME);
 	}
 	
 	public void OnGameEvent( CreateLobbyEvent eventArgs )
 	{
+		Log.Info("Event: " + "CreateLobbyEvent");
 		CurrentState = UiManager.ChangeState(GameState.LOBBY_CREATE);
 	}
 	
 	public void OnGameEvent( JoinLobbyEvent eventArgs )
 	{
+		Log.Info("Event: " + "JoinLobbyEvent");
 		CurrentState = UiManager.ChangeState(GameState.LOBBY_JOIN);
 	}
 	
 	public void OnGameEvent( LeaveLobbyEvent eventArgs )
 	{
+		Log.Info("Event: " + "LeaveLobbyEvent");
 		CurrentState = UiManager.ChangeState(GameState.MENU);
 	}
 
