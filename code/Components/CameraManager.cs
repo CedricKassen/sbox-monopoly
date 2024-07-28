77da063104cc08559d4429da09654313513f4d32
Cedric Kassen <cedkassen@gmail.com>
2024-07-28 17:10:19 +0200

    Implement Camera Changer

--- code/Components/CameraManager.cs
+++ code/Components/CameraManager.cs
@@ -5,9 +5,22 @@ public sealed class CameraManager : Component
 	[Property]
 	public Dictionary<string, CameraComponent> Cameras { get; set; }
 	public string ActiveCamera { get; set; }
+
+	public void SetActiveCamera(string name) {
+		if (Cameras.TryGetValue(name, out var newCamera)) {
+			newCamera.IsMainCamera = true;
+			newCamera.Enabled = true;
+		}
+		
+		if (Cameras.TryGetValue(ActiveCamera, out var oldCamera)) {
+			oldCamera.IsMainCamera = false;
+			oldCamera.Enabled = false;
+		}
+
+		ActiveCamera = name;
+	}
 	
 	protected override void OnUpdate()
 	{
-		
 	}
 }
