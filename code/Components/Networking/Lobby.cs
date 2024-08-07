5be949c21bf24724fabd0d459032e89f9ada7819
FreshDoktor <freshdoktor@freshgaming.eu>
2024-08-07 19:41:10 +0200

    create lobby on load

--- code/Components/Networking/Lobby.cs
+++ code/Components/Networking/Lobby.cs
@@ -1,40 +1,54 @@
-using Sandbox;
+using System.Threading.Tasks;
 using Sandbox.Network;
+using Sandbox.UI;
 
 public sealed class Lobby : Component, Component.INetworkListener
 {
-	[Property]
-	public long MaxPlayers { get; set; } = 5;
-	
-	[Property]
-	public GameObject PlayerPrefab { get; set; }
+	[Property] public long MaxPlayers { get; set; } = 5;
 
-	[Property]
-	public List<Player> Players {
-		get {
-			return new List<Player>(Game.ActiveScene.GetAllComponents<Player>());
-		}
-	}
+	[Property] public GameObject PlayerPrefab { get; set; }
+
+	[Property] public List<Player> Players => new(Game.ActiveScene.GetAllComponents<Player>());
 
 	[Property] public GameObject SpawnLocation { get; set; }
-	
+
 	public void OnActive(Connection conn)
 	{
-		Log.Info( $"Player '{conn.DisplayName}' tritt bei" );
+		Log.Info($"Player '{conn.DisplayName}' tritt bei");
 
-		GameObject playerObj = PlayerPrefab.Clone(SpawnLocation.Transform.World, name: conn.DisplayName + " - Network");
+		var playerObj = PlayerPrefab.Clone(SpawnLocation.Transform.World, name: conn.DisplayName + " - Network");
 		playerObj.Components.Get<Player>().Name = conn.DisplayName;
 		playerObj.Components.Get<Player>().SteamId = conn.SteamId;
 		playerObj.Components.Get<Player>().Connection = conn;
-		
+
 		playerObj.NetworkSpawn(conn);
 	}
 
 	public void OnDisconnected(Connection conn)
 	{
-		IEnumerable<Player> currentPlayers = Scene.GetAllComponents<Player>();
-		Player player = currentPlayers.First(player => player.SteamId == conn.SteamId);
+		var currentPlayers = Scene.GetAllComponents<Player>();
+		var player = currentPlayers.First(player => player.SteamId == conn.SteamId);
 		player.GameObject.Destroy();
 	}
-}
 
+	protected override async Task OnLoad()
+	{
+		if (Scene.IsEditor)
+		{
+			return;
+		}
+
+		if (!GameNetworkSystem.IsActive)
+		{
+			LoadingScreen.Title = "Creating Lobby";
+			await Task.DelayRealtimeSeconds(0.1f);
+			GameNetworkSystem.CreateLobby();
+		}
+	}
+
+	[Broadcast]
+	public void ChangeGameScene(Panel panel, string url)
+	{
+		panel.Navigate(url);
+	}
+}
