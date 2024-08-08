76f40e9acb17c931352fa4b070882fdcf3c1e514
FreshDoktor <freshdoktor@freshgaming.eu>
2024-08-08 23:35:30 +0200

    call new card action manager if MovementDoneEvent was fired

--- code/Components/GameLoop/GameEventHandler.cs
+++ code/Components/GameLoop/GameEventHandler.cs
@@ -3,13 +3,19 @@ using Sandbox.Events.TurnEvents;
 
 namespace Sandbox.Components.GameLoop;
 
-public class GameEventHandler : Component, IGameEventHandler<RolledEvent>
+public class GameEventHandler : Component, IGameEventHandler<RolledEvent>, IGameEventHandler<MovementDoneEvent>
 {
 	[Property] public GameObject LocationContainer { get; set; }
 
 	[Property] public Lobby Lobby { get; set; }
 
 	[Property] public MovementManager MovementManager { get; set; }
+	[Property] public CardActionManager CardActionManager { get; set; }
+
+	public void OnGameEvent(MovementDoneEvent eventArgs)
+	{
+		CardActionManager.DisplayCardFor(GetPlayerFromEvent(eventArgs), eventArgs.Location);
+	}
 
 	public void OnGameEvent(RolledEvent eventArgs)
 	{
