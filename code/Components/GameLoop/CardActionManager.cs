a9a34eb8c59a1ccd70d7dba2403ca86bbd50ca0a
FreshDoktor <freshdoktor@freshgaming.eu>
2024-08-10 16:01:08 +0200

    add card data to IngameStateManager

--- code/Components/GameLoop/CardActionManager.cs
+++ code/Components/GameLoop/CardActionManager.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Security;
 using System.Threading.Tasks;
 using Monopoly.UI.Screens.GameLoop;
 using Sandbox.Constants;
@@ -23,7 +22,6 @@ public sealed class CardActionManager : Component
 
 	private void FillChangeCards()
 	{
-		
 		ChanceCards = new List<Card>(Cards.Chance);
 
 		if (!IsChanceJailCardPresent)
@@ -90,36 +88,38 @@ public sealed class CardActionManager : Component
 
 		Log.Info("Draw Chance: " + card.Text);
 
+		IngameStateManager.Data = card;
+		
 		int targetLocation;
 		switch (card.ActionId)
 		{
 			case 1:
 				// Go to Broadwalk
-				MovementManager.StartMovement(player, 39 - player.CurrentField);
+				MovementManager.StartMovement(player, CalculateFieldsToTravel(player.CurrentField, 39));
 				break;
 			case 2:
 				// Go to Go
-				MovementManager.StartMovement(player, 40 - player.CurrentField);
+				MovementManager.StartMovement(player, CalculateFieldsToTravel(player.CurrentField, 40));
 				break;
 			case 3:
 				// Go to Illinois Avenue
-				MovementManager.StartMovement(player, 40 - player.CurrentField + 24);
+				MovementManager.StartMovement(player, CalculateFieldsToTravel(player.CurrentField, 24));
 				break;
 			case 4:
 				// Go to St. Charles Place
-				MovementManager.StartMovement(player, 11 - player.CurrentField);
+				MovementManager.StartMovement(player, CalculateFieldsToTravel(player.CurrentField, 11));
 				break;
 			case 5:
 			case 6:
 				// Go to next line
 				targetLocation = FindNearestLine(player.CurrentField);
-				MovementManager.StartMovement(player, 40 - player.CurrentField + targetLocation);
+				MovementManager.StartMovement(player, CalculateFieldsToTravel(player.CurrentField, targetLocation));
 				player.Tags.Add(PlayerTags.RAILROAD_EVENT.ToString());
 				break;
 			case 7:
 				// Go to next Utility Field 12 & 28
 				targetLocation = player.CurrentField is < 12 or > 28 ? 12 : 28;
-				MovementManager.StartMovement(player, 40 - player.CurrentField + targetLocation);
+				MovementManager.StartMovement(player, CalculateFieldsToTravel(player.CurrentField, targetLocation));
 				player.Tags.Add(PlayerTags.UTILITY_EVENT.ToString());
 				break;
 			case 8:
@@ -146,7 +146,7 @@ public sealed class CardActionManager : Component
 				break;
 			case 14:
 				// Go to Reading Railroad
-				MovementManager.StartMovement(player, 40 - player.CurrentField + 5);
+				MovementManager.StartMovement(player, CalculateFieldsToTravel(player.CurrentField, 5));
 				break;
 			case 15:
 				// Pay each player
@@ -166,6 +166,16 @@ public sealed class CardActionManager : Component
 		{
 			FillChangeCards();
 		}
+	}
+
+	private int CalculateFieldsToTravel(int currentField, int targetField)
+	{
+		if (currentField < targetField)
+		{
+			return targetField - currentField;
+		}
+
+		return 40 - currentField + targetField;
 	}
 
 	private int FindNearestLine(int playerCurrentField)
