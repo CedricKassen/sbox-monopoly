ad0950dc67f2729e7c72f88a3dc37c6b2f01508e
FreshDoktor <freshdoktor@freshgaming.eu>
2024-08-23 00:01:05 +0200

    use sound handle field instead of TimeSince to check music length

--- code/Components/Util/MusicManager.cs
+++ code/Components/Util/MusicManager.cs
@@ -1,50 +1,39 @@
 using System.Threading.Tasks;
 
 public sealed class MusicManager : BaseSoundComponent {
-	[Property] public float MusicDuration = 238.21062f;
-	[Property] private TimeSince MusicStart;
-	private SoundHandle TempSoundHandle;
+    [Property] public float MusicDuration = 238.21062f;
+    private SoundHandle TempSoundHandle;
 
-	[Property] [Group("Sound")] public float RepeatOffset { get; set; }
-
-
-	protected override Task OnLoad() {
-		foreach (var sound in SoundEvent.Sounds) {
-			sound?.Preload();
-		}
-
-		return base.OnLoad();
-	}
-
-	protected override void OnUpdate() {
-		if (SoundHandle is not null) {
-			ApplyOverrides(SoundHandle);
-		}
-
-		if (TempSoundHandle is not null) {
-			ApplyOverrides(TempSoundHandle);
-		}
-
-		if (MusicDuration <= MusicStart.Relative + RepeatOffset) {
-			MusicStart = 0;
-			SoundEvent.Volume = Volume;
-			TempSoundHandle = Sound.Play(SoundEvent);
-		}
-
-		if (SoundHandle.IsValid() && SoundHandle.IsPlaying) {
-			return;
-		}
+    [Property] [Group("Sound")] public float RepeatOffset { get; set; }
 
 
-		if (!SoundHandle.IsValid() && !TempSoundHandle.IsValid()) {
-			MusicStart = 0;
-			SoundEvent.Volume = Volume;
-			SoundHandle = Sound.Play(SoundEvent);
-		}
+    protected override Task OnLoad() {
+        foreach (var sound in SoundEvent.Sounds) sound?.Preload();
 
-		if (SoundHandle.IsValid() && !SoundHandle.IsPlaying) {
-			SoundHandle = TempSoundHandle;
-			TempSoundHandle = null;
-		}
-	}
+        return base.OnLoad();
+    }
+
+    protected override void OnUpdate() {
+        if (SoundHandle is not null) ApplyOverrides(SoundHandle);
+
+        if (TempSoundHandle is not null) ApplyOverrides(TempSoundHandle);
+
+        if (SoundHandle.IsValid() && MusicDuration <= SoundHandle.ElapsedTime + RepeatOffset) {
+            SoundEvent.Volume = Volume;
+            TempSoundHandle = Sound.Play(SoundEvent);
+        }
+
+        if (SoundHandle.IsValid() && SoundHandle.IsPlaying) return;
+
+
+        if (!SoundHandle.IsValid() && !TempSoundHandle.IsValid()) {
+            SoundEvent.Volume = Volume;
+            SoundHandle = Sound.Play(SoundEvent);
+        }
+
+        if (SoundHandle.IsValid() && !SoundHandle.IsPlaying) {
+            SoundHandle = TempSoundHandle;
+            TempSoundHandle = null;
+        }
+    }
 }
