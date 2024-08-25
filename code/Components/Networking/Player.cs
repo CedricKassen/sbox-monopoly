dc07726ff942a396705d17b75e7df01f0f9f4d23
Cedric Kassen <cedkassen@gmail.com>
2024-08-25 17:36:39 +0200

    hover ui for ownership dots

--- code/Components/Networking/Player.cs
+++ code/Components/Networking/Player.cs
@@ -24,17 +24,26 @@ public sealed class Player : Component {
 		}
 	}
 
-	[Property] [HostSync] public int Money { get; set; } = 2000;
+	[Property, HostSync]
+	public int Money { get; set; } = 2000;
 
-	[Property] [HostSync] public int CurrentField { get; set; }
+	[Property, HostSync]
+	public int CurrentField { get; set; }
 
-	[Property] public Connection Connection { get; set; }
+	[Property]
+	public Connection Connection { get; set; }
 
-	[Property] public IngameUI.LocalUIStates localUiState { get; set; } = IngameUI.LocalUIStates.None;
+	[Property]
+	public IngameUI.LocalUIStates localUiState { get; set; } = IngameUI.LocalUIStates.None;
 
-	[Property] public int LastDiceCount { get; set; } = 0;
+	[Property]
+	public int LastDiceCount { get; set; } = 0;
 
-	[Property] public int DoublesCount { get; set; } = 0;
+	[Property]
+	public int DoublesCount { get; set; } = 0;
+
+	[Property]
+	public GameLocation CurrentHoverLocation { get; set; } = null;
 
 	[Description("0 Means player is not in jail, 1 means the NEXT turn is the first and so forth")]
 	[Property, HostSync]
